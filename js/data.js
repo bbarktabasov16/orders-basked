17fe604d0ac045fe7b36eb97a94d944884ee1e42
Bekzat <118148584+bbarktabasov16@users.noreply.github.com>
2024-03-12 18:06:19 +0600

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,40 @@
+const menu_items = [
+    {
+        id: 1,
+        name: 'burger',
+        price: 150,
+        img: './img/burger.jpeg'
+    },
+    {
+        id: 2,
+        name: 'coffee',
+        price: 30,
+        img: './img/coffee.jpeg'
+    },
+    {
+        id: 3,
+        name: 'cola',
+        price: 75,
+        img: './img/cola.jpeg'
+    },
+    {
+        id: 4,
+        name: 'pizza',
+        price: 450,
+        img: './img/pizza.jpeg'
+    },
+    {
+        id: 5,
+        name: 'potatoes',
+        price: 50,
+        img: './img/potatoes.jpeg'
+    },
+    {
+        id: 6,
+        name: 'tea',
+        price: 20,
+        img: './img/tea.jpeg'
+    },
+]
+
+const orders_basked = []
